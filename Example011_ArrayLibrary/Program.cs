073fdb29a44cf9c892b33b64f354b8d7645c2f32
fenixfire1980 <djonmacane@yandex.ru>
2023-09-21 11:18:09 +0300

    Example011ArrayLibary

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,51 @@
+﻿
+
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index = index + 1;
+    }
+}
+
+void PrintArray(int[] coll)
+{
+    int count = coll.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(coll[position]);
+        position = position + 1;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index = index + 1;
+
+    }
+    return position;
+}
+
+int[] array = new int[10]; //Создай новый масив в котором будут 10 элементов
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 77);
+Console.WriteLine(pos);
